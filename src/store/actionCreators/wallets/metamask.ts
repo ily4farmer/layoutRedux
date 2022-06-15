2150d230b1fbbacbb778c01cfabe7adcf2810337
Lexa <ily4.farmer@yandex.ru>
2022-06-15 13:29:01 +0300

    1

--- src/store/actionCreators/wallets/metamask.ts
+++ src/store/actionCreators/wallets/metamask.ts
@@ -3,23 +3,21 @@ import { Dispatch } from "redux";
 import { WalletActions, WalletActionsTypes } from "../../../types/walletTypes";
 
 
-export const metamask = () => {
-    return async (dispatch: Dispatch<WalletActions>):Promise<void> => {
-        if ((window as any).ethereum) {
-            try {
-                const provider = new providers.Web3Provider((window as any).ethereum)
-                const singer = provider.getSigner()
-                const walletAddress = await singer.getAddress()
-                dispatch({
-                    type: WalletActionsTypes.SET_STATE,
-                    provider: () => provider,
-                    singer: ()=> singer,
-                    walletAddress
-                })
-            } catch (error) {
-                console.log(error);
-                
-            }
+export const metamask = () => async (dispatch: Dispatch<WalletActions>): Promise<void> => {
+    if ((window as any).ethereum) {
+        try {
+            const provider = new providers.Web3Provider((window as any).ethereum)
+            const singer = provider.getSigner()
+            const walletAddress = await singer.getAddress()
+            dispatch({
+                type: WalletActionsTypes.SET_STATE,
+                provider: () => provider,
+                singer: () => singer,
+                walletAddress
+            })
+        } catch (error) {
+            console.log(error);
+
         }
     }
 }
